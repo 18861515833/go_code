205b96db0f1bb8f602152fb01320217b6e9bba7f
wangwenwu <wangwenwu@founq.com>
2019-07-04 10:46:56 +0800

    增加go的多态用法

---
+++ go多态.go
@@ -0,0 +1,40 @@
+//多态测试 利用接口实现
+package main
+
+import "fmt"
+
+type animal interface {
+	getName() string
+	eat()
+}
+type cat struct {
+	name string
+}
+type dog struct {
+	name string
+}
+
+func (c cat) eat()  {
+	fmt.Println(c.getName(),"吃鱼")
+}
+
+func (c cat) getName() string {
+	return c.name
+}
+
+func (c dog) eat()  {
+	fmt.Println(c.getName(),"吃骨头")
+}
+
+func (c dog) getName() string {
+	return c.name
+}
+
+func action(a animal){
+	a.eat()
+}
+
+func main() {
+	action(cat{"猫"})
+	action(dog{"狗"})
+}
